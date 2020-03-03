d9b57bd53f121de7b1ec03283d9d458874555411
tarunpsquare <61342483+tarunpsquare@users.noreply.github.com>
2020-03-03 10:19:24 +0530

    Update emi.c

--- emi.c
+++ emi.c
@@ -9,7 +9,7 @@ printf("Enter the rate of interest: \n");
 scanf("%f\n", &r);
 printf("Enter the number of monthly installments: \n");
 scanf("%f\n", &n);
-emi = (p*r*pow((1+r), n))/(pow((1+r, n)-1));
+emi = (p*r*pow((1+r), n))/(pow(1+r, n)-1);
 printf("The principal amount is: %f\n", p);
 printf("The rate of interest is: %f\n", r);
 printf("The number of monthly installments is/are: %f\n", n);

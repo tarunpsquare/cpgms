1ed2f7a545f07fba2d2f71d7a97421c49e956434
tarunpsquare <61342483+tarunpsquare@users.noreply.github.com>
2020-03-03 09:36:05 +0530

    Add files via upload

---
+++ formultvar.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int counter, i;
+for(counter=0, i=0; i<=5, counter<=10; i++, counter++)
+{
+printf("%d%d\n", counter, i);
+
+}
+}
---
+++ formultvarinfinite.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int counter, i;
+for(;;)
+{
+printf("%d\n", counter);
+printf("%d\n", i);
+}
+}

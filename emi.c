33cab3df5e5907cc1c3fa06f4e908831fae231c2
tarunpsquare <61342483+tarunpsquare@users.noreply.github.com>
2020-03-03 06:11:42 +0530

    Update emi.c

--- emi.c
+++ emi.c
@@ -3,15 +3,16 @@
 void main()
 {
 float p, r, n, emi;
-
-printf("Enter the rate of interest: \n");
-scanf("%f\n", &r);
 printf("Enter the Principal amount: \n");
 scanf("%f\n", &p);
+printf("Enter the rate of interest: \n");
+scanf("%f\n", &r);
 printf("Enter the number of monthly installments: \n");
 scanf("%f\n", &n);
 emi = (pow((p*r*(1+r)), n))/(pow((1+r), (n-1)));
-
+printf("The principal amount is: %f\n", p);
+printf("The rate of interest is: %f\n", r);
+printf("The number of monthly installments is/are: %f\n", n);
 printf("EMI is: %f\n", emi);
 }
 

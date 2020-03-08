6d878e2fffa36d2623e50f80a7371e4638ea3efb
tarunpsquare <61342483+tarunpsquare@users.noreply.github.com>
2020-03-08 16:12:33 +0530

    Update marks2.c

--- marks2.c
+++ marks2.c
@@ -2,16 +2,16 @@
 void main()
 {
 char name[20];
-float m1, m2, m3, total, avg;
+float m1, m2, m3, m4, m5 total, avg;
 printf("Enter the name of the student: \n");
 scanf("%s", name);
 printf("Enter marks : \n");
-scanf("%f %f %f", &m1, &m2, &m3);
+scanf("%f %f %f %f %f", &m1, &m2, &m3, &m4, &m5);
 printf("Name is: %s \n", name);
-printf("Marks in subjects are: %f %f %f\n", m1, m2, m3);
+printf("Marks in subjects are: %f %f %f %f %F\n", m1, m2, m3, m4, m5);
 
-total=m1+m2+m3;
-avg=total/3.0;
+total=m1+m2+m3+m4+m5;
+avg=total/5.0;
 
 printf("Total is: ");
 printf("%f \n", total);
